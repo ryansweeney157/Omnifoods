fe258dfdcd9b4346dcdc63468313495cc738cc6e
RyanS <ryanweeney157@gmail.com>
2024-10-08 23:14:19 -0400

    finished everything

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var mobileNav = document.getElementById("mobile-nav");
+    if (mobileNav.style.display === "block") {
+        mobileNav.style.display = "none";
+    } else {
+        mobileNav.style.display = "block";
+    }
+}
